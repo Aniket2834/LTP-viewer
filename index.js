427454ab625994af8998c74a16b27e935f918211
aniket2834 <malianiket2525@gmail.com>
2025-02-19 15:53:04 +0530

    change in index.js

--- index.js
+++ index.js
@@ -79,15 +79,15 @@ app.use("/api/stocks", authMiddleware, datachartRouter);
 //   console.log("running development server");
 // }
 
-if (prod) {
-  const fp = config.FRONTEND_PATH;
-  app.use(morgan("dev"));
-  app.use("/", express.static(fp));
-  app.get("/*", (req, res) => {
-    res.sendFile("index.html", { root: config.FRONTEND_PATH });
-  });
-  console.log("starting production server");
-}
+const FRONTEND_PATH =
+  process.env.FRONTEND_PATH || "https://ltp-viewer-frontend.onrender.com";
+
+app.use(
+  cors({
+    origin: FRONTEND_PATH, // Allow only frontend
+    credentials: true, // Required if using authentication cookies
+  })
+);
 
 //not found
 app.use("*", (req, res) => {
