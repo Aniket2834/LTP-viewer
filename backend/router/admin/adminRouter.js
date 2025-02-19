1fdba9b31b13c320f50d6a95ecd42f7e93fadbbd
aniket2834 <malianiket2525@gmail.com>
2025-02-19 14:19:44 +0530

    changes

--- backend/router/admin/adminRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import adminuserRouter from './adminuserRouter.js'
-
-const adminRouter = Router()
-
-export default adminRouter
-
-adminRouter.use('/user', adminuserRouter)
-
